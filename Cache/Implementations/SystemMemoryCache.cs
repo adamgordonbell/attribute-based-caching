90c0fd1db005256cf8c893b6e83e345925451924
DarekDan <dydek@hotmail.com>
2014-08-19 18:41:59 -0500

    Updated version number

---
+++ Cache/Implementations/SystemMemoryCache.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Caching;
+
+namespace CacheAspect
+{
+    public class SystemMemoryCache:ICache
+    {
+        public object this[string key]
+        {
+            get { return MemoryCache.Default[key]; }
+            set { MemoryCache.Default[key] = value; }
+        }
+
+        public bool Contains(string key)
+        {
+            return MemoryCache.Default.Contains(key);
+        }
+
+        public void Delete(string key)
+        {
+            MemoryCache.Default.Remove(key);
+        }
+
+        public void Clear()
+        {
+            foreach (KeyValuePair<string, object> kvp in MemoryCache.Default)
+            {
+                MemoryCache.Default.Remove(kvp.Key);
+            }
+            
+        }
+    }
+}
