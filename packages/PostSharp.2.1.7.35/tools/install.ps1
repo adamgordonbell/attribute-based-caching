359b7a0714e1c79d9930cbe4e996512d937de820
DarekDan <dydek@hotmail.com>
2015-06-25 09:22:34 -0500

    Merging changes from Visual Studio Online

---
+++ packages/PostSharp.2.1.7.35/tools/install.ps1
@@ -0,0 +1,52 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+
+function PathToUri([string] $path)
+{
+    return new-object Uri('file://' + $path.Replace("%","%25").Replace("#","%23").Replace("$","%24").Replace("+","%2B").Replace(",","%2C").Replace("=","%3D").Replace("@","%40").Replace("~","%7E").Replace("^","%5E"))
+}
+
+function UriToPath([System.Uri] $uri)
+{
+    return [System.Uri]::UnescapeDataString( $uri.ToString() ).Replace([System.IO.Path]::AltDirectorySeparatorChar, [System.IO.Path]::DirectorySeparatorChar)
+}
+
+$targetsFile = [System.IO.Path]::Combine($toolsPath, 'PostSharp.targets')
+
+# Need to load MSBuild assembly if it's not loaded yet.
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+
+# Grab the loaded MSBuild project for the project
+$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+
+# Make the path to the targets file relative.
+$projectUri = PathToUri $project.FullName
+$targetUri = PathToUri $targetsFile
+
+$relativePath = UriToPath $projectUri.MakeRelativeUri($targetUri)
+
+# Remove previous properties DontImportPostSharp
+$msbuild.Xml.Properties | Where-Object {$_.Name.ToLowerInvariant() -eq "dontimportpostsharp" } | Foreach { 
+	$_.Parent.RemoveChild( $_ ) 
+	"Removed property DontImportPostSharp"
+}
+
+# Remove previous imports to PostSharp.targets
+$msbuild.Xml.Imports | Where-Object {$_.Project.ToLowerInvariant().EndsWith("postsharp.targets") } | Foreach { 
+	$_.Parent.RemoveChild( $_ ) 
+	[string]::Format( "Removed import of '{0}'" , $_.Project )
+}
+
+# Add the property DontImportPostSharp
+$msbuild.Xml.AddProperty( "DontImportPostSharp", "True" ) | Out-Null
+"Added property DontImportPostSharp=true."
+
+# Add import to PostSharp.targets
+$import = $msbuild.Xml.AddImport($relativePath)
+$import.set_Condition( "Exists('$relativePath')" ) | Out-Null
+[string]::Format("Added import of '{0}'.", $relativePath )
+
+# Check if we should install the VSX add-in.
+$vsxInstaller = [System.IO.Path]::Combine($toolsPath, 'Release\PostSharp.HQ.exe')
+Start-Process -FilePath $vsxInstaller -ArgumentList @("/first-setup")
+	
